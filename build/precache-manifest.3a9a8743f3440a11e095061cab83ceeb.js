06d440b28869917990f27ad7082f1175b3023ef1
Andy Luong <andyluong@Andys-MBP.hsd1.ca.comcast.net>
2022-08-10 00:20:21 -0700

    add imgs

---
+++ build/precache-manifest.3a9a8743f3440a11e095061cab83ceeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97387ff3c89d40310a5a",
+    "url": "/ReactWebsite/static/css/main.633db6aa.chunk.css"
+  },
+  {
+    "revision": "97387ff3c89d40310a5a",
+    "url": "/ReactWebsite/static/js/main.97387ff3.chunk.js"
+  },
+  {
+    "revision": "c493fdcf6a139090962f",
+    "url": "/ReactWebsite/static/js/1.c493fdcf.chunk.js"
+  },
+  {
+    "revision": "0094d59ab3b60f163e47",
+    "url": "/ReactWebsite/static/js/runtime~main.0094d59a.js"
+  },
+  {
+    "revision": "85403051845beee4e7da7fe09d6ac9d8",
+    "url": "/ReactWebsite/index.html"
+  }
+];
