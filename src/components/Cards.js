552628b5dfd0bcbf5eedf4c5513ceed7ce7f82b2
Andy Luong <andyluong@Andys-MBP.hsd1.ca.comcast.net>
2022-08-10 00:29:06 -0700

    fdfsda

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Database project storing information'
+              src='./images/img-9.jpg'
+              text='Database project storing info'
               label='Main'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='./images/img-2.jpg'
               text='Building a professional website through React'
               label='Main'
               path='/services'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='./images/img-3.jpg'
               text='Making a mobile app'
               label='Secondary'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='./images/img-4.jpg'
               text='Developing a website through basic HTML, CSS, & Javascript'
               label='Secondary'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='./images/img-8.jpg'
               text='Coding a game under Unreal Engine 5'
               label='Secondary'
               path='/sign-up'
