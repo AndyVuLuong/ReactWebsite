2340f0e372dbf0a18384f92211610e7cf13bf49e
Andy Luong <andyluong@Andys-MBP.hsd1.ca.comcast.net>
2022-08-16 03:56:18 -0700

    a

---
+++ build/precache-manifest.776abf8d9884ec159548ce114d1e9808.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a91b72c5277eab6e4d32",
+    "url": "/ReactWebsite/static/css/main.08f52403.chunk.css"
+  },
+  {
+    "revision": "a91b72c5277eab6e4d32",
+    "url": "/ReactWebsite/static/js/main.a91b72c5.chunk.js"
+  },
+  {
+    "revision": "ad1efb76f8f1ae4b045e",
+    "url": "/ReactWebsite/static/js/1.ad1efb76.chunk.js"
+  },
+  {
+    "revision": "0094d59ab3b60f163e47",
+    "url": "/ReactWebsite/static/js/runtime~main.0094d59a.js"
+  },
+  {
+    "revision": "0a691dd015e231824196bc6d45a0c521",
+    "url": "/ReactWebsite/static/media/mobile-app.0a691dd0.jpg"
+  },
+  {
+    "revision": "3a9b84e352c4dbfe1437eed88e4b743b",
+    "url": "/ReactWebsite/static/media/titanic-dataset.3a9b84e3.jpg"
+  },
+  {
+    "revision": "b9e581830efdec21d339d584def42bd8",
+    "url": "/ReactWebsite/static/media/personal-website.b9e58183.png"
+  },
+  {
+    "revision": "bb9c7e2f38c03319685c3181c73adb24",
+    "url": "/ReactWebsite/static/media/climate.bb9c7e2f.png"
+  },
+  {
+    "revision": "868ef95c118185b9cd917ec36aee5a3e",
+    "url": "/ReactWebsite/static/media/unreal.868ef95c.png"
+  },
+  {
+    "revision": "432de0fb593369129f132ee8db054d3e",
+    "url": "/ReactWebsite/index.html"
+  }
+];
