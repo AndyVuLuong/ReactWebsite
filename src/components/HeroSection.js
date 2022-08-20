7310d50495e00efaa49f4219ea635be934ce613a
Andy Luong <andyluong67@gmail.com>
2022-08-20 15:47:21 -0700

    a

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,13 +3,14 @@ import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
 import { Link } from 'react-router-dom';
+import homecover from './images/SJSU.jpg';
 
 //<video src='/videos/video-1.mp4' autoPlay loop muted />
 //<img src='/images/SJSU.jpg' alt='SJSU'/>
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <img src='/images/SJSU.jpg' alt='SJSU' />
+      <img src={homecover} alt='SJSU' />
       <h1>Andy Luong</h1>
       <p>San Jose State University: Computer Science</p>
       <div className='hero-btns'>
